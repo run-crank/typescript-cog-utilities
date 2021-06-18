d6646fac0122cccf4910fa085291b1cfb0ed4740
Angelito Salud <asalud@you-source.com>
2021-06-18 13:24:16 +0800

    Added match and does not match regex operators

---
+++ src/constants/valid-operators.ts
@@ -0,0 +1,14 @@
+export const validOperators: string[] = [
+  'be',
+  'not be',
+  'contain',
+  'not contain',
+  'be greater than',
+  'be less than',
+  'be set',
+  'not be set',
+  'be one of',
+  'not be one of',
+  'matches',
+  'does not match',
+];
